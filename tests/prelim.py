fd31abdc8c9430fe64040636f8c74e59a609cab3
krittin-kulrat <63114856+krittin-kulrat@users.noreply.github.com>
2023-12-22 02:02:27 -0800

    Add `tests/prelim.py` to temperarily pass the unittest

---
+++ tests/prelim.py
@@ -0,0 +1,15 @@
+
+
+import unittest
+
+
+class Testnothing(unittest.TestCase):
+    """This class is here to pass the unittest
+    Since currently there is no test available
+    Delete this file after got first testcase
+    """
+
+    def test_pass(self):
+        """This test is designed to pass the unitest
+        """
+        self.assertTrue(True)
